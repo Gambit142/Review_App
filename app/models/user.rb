10b4db7dde8e0914e94b30ca47b744c1dbe19947
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-10-05 16:18:06 +0100

    Add validations for attributes in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :name, presence: {message: "Name can't be blank"}
+  validates :email, presence: {message: "Email can't be blank"}, 
+            uniqueness: {message: "This Email already exists"}, email: true
 end
