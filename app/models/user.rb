70ddf1c2bc3afae983ace4dcd612f456fe00ce0e
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-10-06 22:54:34 +0100

    Correct validations for email attribute in user's model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_secure_password
   validates :name, presence: {message: "Name can't be blank"}
   validates :email, presence: {message: "Email can't be blank"}, 
-            uniqueness: {message: "This Email already exists"}, email: true
+            uniqueness: {message: "This Email already exists"}
 end
