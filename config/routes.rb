6a760edf06ef6ea7d5b3819b4c65c8eb86104629
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-10-06 22:56:11 +0100

    Create routes for user sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
+  post "/signup", to: "users#create", format: 'json'
+  post "/login", to: "sessions#create", format: 'json'
+  get "/authorized", to: "sessions#show", format: 'json'
   root "root#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
