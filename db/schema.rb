472305334913dbf32a50c63f3c56ffc8c2e09e56
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-10-21 23:43:48 +0100

    Run migrations to update schema for database

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_05_144012) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_21_000316) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.string "food_details"
+    t.string "image"
+    t.text "steps"
+    t.text "ingredients"
+    t.bigint "restaurant_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_foods_on_restaurant_id"
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "country_code"
+    t.string "country"
+    t.string "continent"
+    t.string "address"
+    t.string "email"
+    t.string "website"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -23,4 +49,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_05_144012) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "foods", "restaurants"
 end
