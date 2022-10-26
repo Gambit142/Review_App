4d8158b6a5e1fc69629f1e7a8898216cad86b18c
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-10-26 23:13:54 +0100

    Change column names manually

--- db/migrate/20221021000316_create_foods.rb
+++ db/migrate/20221021000316_create_foods.rb
@@ -3,7 +3,7 @@ class CreateFoods < ActiveRecord::Migration[7.0]
     create_table :foods do |t|
       t.string :name
       t.string :food_details
-      t.string :image
+      t.string :image_url
       t.text :steps
       t.text :ingredients
       t.references :restaurant, null: false, foreign_key: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_21_000316) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_24_174831) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
     t.string "food_details"
-    t.string "image"
+    t.string "image_url"
     t.text "steps"
     t.text "ingredients"
     t.bigint "restaurant_id", null: false
@@ -40,6 +40,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_21_000316) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "comment"
+    t.integer "likes"
+    t.string "reviewable_type", null: false
+    t.bigint "reviewable_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["reviewable_type", "reviewable_id"], name: "index_reviews_on_reviewable"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -50,4 +62,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_21_000316) do
   end
 
   add_foreign_key "foods", "restaurants"
+  add_foreign_key "reviews", "users"
 end
