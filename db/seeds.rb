a0c4cb2603217e3bc6fa2559049988575b60fb84
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-10-31 23:52:24 +0100

    Save data from JSON files to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Create Restaurants and Foods
+
+def create_restaurants_and_foods(category_name)
+  restaurants = Oj.load_file("json/restaurants/#{category_name}_restaurants.json")
+  cuisine = Oj.load_file("json/cuisines/#{category_name}_dishes.json")
+  restaurants.each do |restaurant|
+    res = Restaurant.create(**restaurant)
+    res.foods.create_with(created_at: Time.now, updated_at: Time.now).insert_all(cuisine)
+  end
+end
+
+create_restaurants_and_foods("african")
