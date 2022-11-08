f884deac025c63df380e96493f4367bc10e35ea7
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-11-08 23:48:31 +0100

    Create different actions for Review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,34 @@
 class ReviewsController < ApplicationController
+  before_action :get_reviewable, only: [:create, :show_reviews]
+
+  def show_reviews
+    @reviews = @reviewable.reviews
+    render json: @reviews
+  end
+
+  def user_reviews
+    @user = current_user
+    @reviews = @user.reviews
+  end
+
+  def create
+    @review = @reviewable.reviews.new(review_params)
+    @review.user = current_user
+    if @review.save
+      render json: @review
+    else
+      render json: @review.errors.full_messages, status: 422
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :comment)
+  end
+
+  def get_reviewable
+    type = params[:type].singularize.capitalize.constantize
+    @reviewable = type.find(params[:id])
+  end
 end
