2f065dae29657526f57e195d47482435dde71df4
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-11-08 23:48:05 +0100

    Create different actions for Restaurant controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,11 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+    render json: @restaurants
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id]).includes(:foods)
+    render json: @restaurant
+  end
 end
