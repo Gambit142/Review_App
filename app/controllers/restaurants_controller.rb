d65f863db53f35664e05f01411109631ae3802e8
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-11-09 23:14:41 +0100

    Write logic for create action in restaurant controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,4 +8,20 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id]).includes(:foods)
     render json: @restaurant
   end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      render json: @restaurant
+    else
+      render json: @restaurant.errors.full_messages, status: 422
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :description, :country_code, 
+                  :country, :continent, :address, :email, :website, :image)
+  end
 end
