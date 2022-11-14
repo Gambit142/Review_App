da7b80280a124d7d9ba96f5de157fc410e3ebf5f
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-11-14 22:11:06 +0100

    Add conditional statements for actions in foods controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,14 +1,24 @@
 class FoodsController < ApplicationController
   before_action :find_restaurant, only: [:index, :show]
   def index
-    @foods = @restaurant.foods
-    render json: @foods
+    if logged_in?
+      @foods = @restaurant.foods
+      render json: @foods
+    else
+      render json: {error: "You must be logged in to view this page"}
+    end
   end
 
   def show
-    @food = @restaurant.foods.find(params[:id])
-    render json: @food
+    if logged_in?
+      @food = @restaurant.foods.find(params[:id])
+      render json: @food
+      else
+        render json: {error: "You must be logged in to view this page"}
+    end
   end
+
+  private
 
   def find_restaurant
     @restaurant = Restaurant.find(params[:restaurant_id])
