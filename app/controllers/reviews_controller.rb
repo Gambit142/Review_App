95f4482992b92ee46dcc782e26d4d9d0751a4c4d
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-11-14 22:11:32 +0100

    Add conditional statements for actions in reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,22 +2,34 @@ class ReviewsController < ApplicationController
   before_action :get_reviewable, only: [:create, :show_reviews]
 
   def show_reviews
-    @reviews = @reviewable.reviews
-    render json: @reviews
+    if logged_in?
+      @reviews = @reviewable.reviews
+      render json: @reviews
+    else
+      render json: {error: "You must be logged in to view this page"}
+    end
   end
 
   def user_reviews
-    @user = current_user
-    @reviews = @user.reviews
+    if logged_in?
+      @user = current_user
+      @reviews = @user.reviews
+    else
+      render json: {error: "You must be logged in to view this page"}
+    end
   end
 
   def create
-    @review = @reviewable.reviews.new(review_params)
-    @review.user = current_user
-    if @review.save
-      render json: @review
+    if logged_in?
+      @review = @reviewable.reviews.new(review_params)
+      @review.user = current_user
+      if @review.save
+        render json: @review
+      else
+        render json: @review.errors.full_messages, status: 422
+      end
     else
-      render json: @review.errors.full_messages, status: 422
+      render json: {error: "You must be logged in to view this page"}
     end
   end
 
